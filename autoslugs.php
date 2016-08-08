9f14876fd434a4523c9653362e262d40f06866a2
catrymer <cat@automattic.com>
2016-08-08 17:42:17 -0600

    Update autoslugs.php

--- autoslugs.php
+++ autoslugs.php
@@ -4,7 +4,7 @@ Plugin Name: Autoslugs
 Description: A plugin to create easy to read post slugs for posts that don't have titles.
 Version:     1.0
 Author:      Cat Rymer
-License:     GPL2
+License:     GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 */
 
